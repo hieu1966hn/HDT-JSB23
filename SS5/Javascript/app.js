c565784e30da5ee21445ec81e96bc5ad2decdff2
hieu1966hn <hieu1966hn@gmail.com>
2021-09-04 16:37:17 +0700

    new code app.js

--- SS5/Javascript/app.js
+++ SS5/Javascript/app.js
@@ -1,1 +1,9 @@
 //  trong này mình sẽ viết code Javascript
+
+// console.log("Hello World!!");
+
+
+// khai báo thành công biến x (x không có giá trị gì cả)
+let x = 1;
+console.log(x); // in ra thành công biến x
+
