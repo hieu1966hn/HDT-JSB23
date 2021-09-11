024a4dd54d3c251e37a18003a837c8fb5a6635c4
hieu1966hn <hieu1966hn@gmail.com>
2021-09-11 18:03:32 +0700

    done bài SS5

--- SS6/Chữa BTVN SS5/app.js
+++ SS6/Chữa BTVN SS5/app.js
@@ -6,5 +6,16 @@ let lastName = "Nguyễn";
 let surName = 'Trung';
 let firstName = `Hiếu`;
 
-console.log(lastName + surName + firstName);
+console.log(lastName + " " + surName + " " + firstName);
+// dấu cộng ở đây là cú pháp cộng chuỗi
 
+
+
+// Bai 02: 
+console.log(6 % 5); // in ra kết quả là: 
+
+
+// Bài 03: tính độ dài của String (chuỗi)
+console.log("Độ dài của biến lastName là:", lastName.length);
+console.log("Độ dài của biến surName là:", surName.length);
+console.log("Độ dài của biến firstName là:", firstName.length);
--- SS6/SS6-lythuyet.md
+++ SS6/SS6-lythuyet.md
@@ -1,4 +1,11 @@
 Có 3 cách để viết chuỗi trong JS
+
 1. "": ngoặc kép
 2. '': ngoặc đơn
 3. ``: Ngoặc huyền.
+
+- Để lấy số dư của 1 phép tính, ta sử dụng toán tử: %
+  VD: 6%5 => kết quả trả về là số du của phép toán
+- Để lấy kết quả chia của 1 phép tính, ta sử dụng toán tử: /
+
+- Bài tính độ dài của String (chuỗi): ta sử dụng phương thức chuoi.lenght
