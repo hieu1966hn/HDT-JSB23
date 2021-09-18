354d6fc57e7a301794d01573a5d665070f973c1a
hieu1966hn <hieu1966hn@gmail.com>
2021-09-18 18:12:30 +0700

    xong bai 2

--- SS7/Chữa btvn SS6/app.js
+++ SS7/Chữa btvn SS6/app.js
@@ -11,13 +11,26 @@
 // console.log(Hieu.name);
 // console.log(Hieu.age);
 // console.log(Hieu.address);
-
+let x = "tuoi"
 let fruits = [
   ["Nho", 15],
   ["Dưa hấu", 10],
   ["Dưa leo", 1],
 ]
 
+
+
 for (let i = 0; i < fruits.length; i = i + 1) {
   console.log(`Tôi có ${fruits[i][1]} quả ${fruits[i][0]}`);
 }
+
+for (let key in fruits) {
+  // console.log(key);
+  console.log(`Tôi có ${fruits[key][1]} quả ${fruits[key][0]}`);
+}
+
+for (let key of fruits) {
+  // console.log(key);
+  console.log(`Tôi có ${key[1]} quả ${key[0]}`);
+}
+
