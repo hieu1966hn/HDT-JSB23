087145223335840e102d928cb7c8e49b9fcf74ab
hieu1966hn <hieu1966hn@gmail.com>
2021-09-25 17:54:31 +0700

    done object

--- SS8/app.js
+++ SS8/app.js
@@ -19,4 +19,15 @@ Nguoi.name = "Thắng";
 Nguoi.age = 15;
 Nguoi.id = "0123456789"
 console.log(Nguoi);
-//
+// Thêm phương thức (hàm) vào cho đối tượng;
+Nguoi.go = function () {
+  alert("go go go"); // câu lệnh thông báo trên web
+}
+
+Nguoi.say = function () {
+  alert("Hello World");
+}
+
+// gọi tới phương thức trong obj (Object: đối tượng)
+Nguoi.say();
+Nguoi.go();
